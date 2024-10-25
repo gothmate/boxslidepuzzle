ade5656b940dbfd333e6fea7b0c3ebf567e81499
gothmate <gothmate@gmail.com>
2024-10-25 13:36:03 -0300

    fix(UI): - alinhando modo mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
-      <main>
+      <main className={styles.main}>
         <GameBox />
       </main>
       <Footer />
