3df39ecd4af5c1c6d1205b3820774afd758f3c9e
gothmate <gothmate@gmail.com>
2024-10-29 08:59:48 -0300

    fix(ui): - Ajuste no ui - Criação do mixin flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
 		<div className={styles.container}>
 			<div className={styles.bloco}>
-				<h2>O Desafio dos Quebra-Cabeças Deslizantes</h2>
+				<h3>O Desafio dos Quebra-Cabeças Deslizantes</h3>
 				<p>
 					Imagine um jogo que desafia o seu raciocínio lógico, paciência e
 					habilidade de planejamento...
@@ -31,26 +31,31 @@ export default function Home() {
 				</p>
 			</div>
 			<div className={styles.bloco}>
-
-        <h3>Desafio:</h3>
-        <p>
-          Concluir o jogo no menor número de movimentos. Curiosidade Sabia que o
-          Box Slide tem raízes que remontam aos quebra-cabeças japoneses do século
-          19? Esse tipo de jogo tem até um apelido lá: “Hakoiri Musume”, que
-          significa algo como filha confinada em uma caixa. A ideia era criar uma
-          solução estratégica para um problema aparentemente simples, mas com
-          infinitas possibilidades de resolução! Por Que Jogar Box Slide? Além de
-          ser uma ótima maneira de passar o tempo, jogar Box Slide ajuda a:
-          Desenvolver habilidades de resolução de problemas. Melhorar o pensamento
-          estratégico. Treinar a paciência e o foco. Conclusão O Box Slide é muito
-          mais que um jogo – é um exercício para o cérebro! Se você acha que está
-          preparado para o desafio, venha experimentar e descobrir o quão ágil e
-          estratégico você consegue ser.
-        </p>
-      </div>
+				<h3>Desafio:</h3>
+				<p>
+				Concluir o jogo no menor número de movimentos. Curiosidade Sabia que o
+				Box Slide tem raízes que remontam aos quebra-cabeças japoneses do século
+				19? Esse tipo de jogo tem até um apelido lá: “Hakoiri Musume”, que
+				significa algo como filha confinada em uma caixa. A ideia era criar uma
+				solução estratégica para um problema aparentemente simples, mas com
+				infinitas possibilidades de resolução! Por Que Jogar Box Slide? Além de
+				ser uma ótima maneira de passar o tempo, jogar Box Slide ajuda a:
+				Desenvolver habilidades de resolução de problemas. Melhorar o pensamento
+				estratégico. Treinar a paciência e o foco.
+				</p>
+			</div>
+			<div className={styles.bloco}>
+				<h3>Conclusão:</h3>
+				<p>
+					O Box Slide é muito
+					mais que um jogo – é um exercício para o cérebro! Se você acha que está
+					preparado para o desafio, venha experimentar e descobrir o quão ágil e
+					estratégico você consegue ser.
+				</p>
+			</div>
 			<Link className={styles.link} href='/game'>
 				Jogar
 			</Link>
 		</div>
-	);
+	)
 }
